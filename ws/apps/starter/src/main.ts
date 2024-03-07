88a0477021ee21356e4452c7fc68587ac1241e0d
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-03-07 14:27:52 -0800

    Server with mocked playlists

--- ws/apps/starter/src/main.ts
+++ ws/apps/starter/src/main.ts
@@ -3,6 +3,8 @@ import { readFileSync } from "fs";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { gql } from "graphql-tag";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const typeDefs = gql(
   readFileSync(path.resolve("./apps/starter/src/schema.graphql"), {
@@ -10,8 +12,26 @@ const typeDefs = gql(
   }),
 );
 
+const mocks = {
+  Query: () => ({
+    playlistsFeatured: () => [...new Array(6)],
+  }),
+  Playlist: () => ({
+    id: () => "playlist01",
+    name: () => "Groovin' with GraphQL",
+    description: () =>
+      "Serving up the hottest development hits, Groovin' with GraphQL has everything you need to get into the coding mindspace... and stay there!",
+  }),
+};
+
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
+
   const { url } = await startStandaloneServer(server);
 
   console.log(`
